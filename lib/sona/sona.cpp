c078f5003096159d85b33d1f3278dfb7ec8ac382
SilasYoome <71079352+SilasYoome@users.noreply.github.com>
2021-04-16 15:07:11 +0800

    remove extra loop

--- lib/sona/sona.cpp
+++ lib/sona/sona.cpp
@@ -1,10 +1,12 @@
-#include "sona.h"
+#include "Sona.h"
 #include "Arduino.h"
 #include <string.h>
 
 Sona::Sona(int _uart_num,int _uart_baud):uart_num(_uart_num),uart_baud(_uart_baud){
     sona_data_buffer[10] = {0};
     sona_data_buffer_flag = 0;
+    check_num = 0;
+    message_tail = 9;
 
 }
 void Sona::init(){
@@ -13,39 +15,45 @@ void Sona::init(){
     }
 }
 
-void Sona::data_calculation(){
-    strncpy(sona_data, sona_data_buffer);
+void Sona::get_data(unsigned char* data,int data_size){
+    if(data_size == sizeof(sona_data)){
+        //僅將sona[1] ~ sona[8]傳出，[0]與[9]是標頭標尾，不需要傳出
+        for(int i = 1;i<data_size;i++){
+            data[i] = sona_data[i]++;
+        }
+    }
 }
 
-void Sona::get_data(){
-    
-}
 
-
-void Sona::uart2_data(){
+bool Sona::uart2_data(){
     while (Serial2.available())
     {
-        sona_data_buffer[sona_data_buffer_flag] = Serial2.read();
+        sona_data_buffer[sona_data_buffer_flag] = Serial2.read(); //uart2接收資料依序放進sona_data_buffer
+
         if(sona_data_buffer_flag == 0 && sona_data_buffer[sona_data_buffer_flag] != 0xFF){
+            //如果標頭不為0xFF時，退回0重新放資料
             sona_data_buffer_flag = 0;
             break;
         } else{
+            
+            check_num += sona_data_buffer[sona_data_buffer_flag];
+
+            //如果沒有觸發標頭錯誤的條件時，sona_data_buffer_flag繼續加上去
             sona_data_buffer_flag++;
-            if(sona_data_buffer_flag > 9){
-                char check_num = 0;
-                for(int i = 0;i<10;i++){
-                    check_num += sona_data_buffer[i];
+
+            //當sona_data_buffer_flag>9，表示buffer放滿，開始做資料處理
+            if(sona_data_buffer_flag > message_tail){
+
+                 //確認標尾正確後放入另外一個空間sona_data，用來存正確的資料
+                if(check_num-sona_data_buffer[sona_data_buffer_flag] == sona_data_buffer[sona_data_buffer_flag]){
+                    memcpy(sona_data,sona_data_buffer,sizeof(sona_data_buffer));
                 }
-                if(check_num == sona_data_buffer[9]){
-                    data_calculation();
-                }
-                check_num = 0;
-                sona_data_buffer_flag = 0;
+                check_num = 0;//確認碼暫存空間歸零
+                sona_data_buffer_flag = 0;//旗標歸0
             }
         }
+        return true;
     }
+    return false;
     
-}
-
-void serialEvent2(){
 }
--- lib/sona/sona.h
+++ lib/sona/sona.h
@@ -4,16 +4,17 @@
 class Sona{
     public:
         Sona(int _uart_num,int _uart_baud);
-        void init();
-        void get_data();
-        void data_calculation();
-        void uart2_data();
+        void init();//初始化
+        void get_data(unsigned char* data,int data_size) ;//將sona_data回傳
+        bool uart2_data();//uart2中斷接收
     private:
-        int uart_num;
-        int uart_baud;
-        int sona_data_buffer_flag;
-        unsigned char sona_data_buffer[10];
-        unsigned char sona_data[10];
+        int uart_num;//uart port 預設是uart2
+        int uart_baud;//uart 鮑率
+        int sona_data_buffer_flag;//sona_data_buffer儲存旗標
+        int message_tail;
+        unsigned char check_num;
+        unsigned char sona_data_buffer[10];//sona接收資料暫存區
+        unsigned char sona_data[10];//確認資料正確後的sona資料存放區
     public:
         
 };
